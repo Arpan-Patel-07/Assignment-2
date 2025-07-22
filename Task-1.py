527e5ac9e7a52b586c9c33e59165add5e74b46da
Arpan Patel 07 <abdhankecha802@gmail.com>
2025-07-23 00:09:09 +0530

    Task -1

---
+++ Task-1.py
@@ -0,0 +1,11 @@
+#Program that takes an integer from user and tels if integer is odd or even
+
+num = int(input("Enter a number : "))
+
+
+
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+    
+else:
+    print(f"{num} is an odd number.")
